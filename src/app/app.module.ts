ae6bc30bb6c123efb3d0b576a0fd5b981533baf2
breno-do-val <brenorvale@gmail.com>
2021-01-04 17:48:48 -0300

    (feat): add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
